d28f8f09c01e0b10e09b4244baa3fee67270c485
Amir Hossein Parhizgar <amparhizgar@gmail.com>
2023-04-28 23:48:23 +0330

    added react files

--- src/main/java/ie/baloot5/Baloot5Application.java
+++ src/main/java/ie/baloot5/Baloot5Application.java
@@ -1,11 +1,7 @@
 package ie.baloot5;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Baloot5Application {
